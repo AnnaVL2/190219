7f29810a60615c5ec632ef34455f1e14a40e173e
AnnaVL2 <43822073+AnnaVL2@users.noreply.github.com>
2019-02-19 19:37:44 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("please enter two numbers");
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
+            if (a > b)
+            {
+                Console.WriteLine(a);
+            }
+           else
+            {
+                Console.WriteLine(b);
+
+            }
+        }
+    }
+}
